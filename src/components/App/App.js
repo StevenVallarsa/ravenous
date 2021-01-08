c3269d85b90fe1f804958011534ac69423c977cd
StevenVallarsa <orionsjv@gmail.com>
2021-01-07 20:25:30 -0500

    converted class to funcational components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <SearchBar />
-      {/* <BusinessList /> */}
+      <BusinessList />
     </div>
   );
 }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -7,7 +7,7 @@ const sortByOptions = {
   "Most Reviewed": "review_count"
 }
 
-class SearchBar extends React.Component {
+function SearchBar() {
 
   // renderSortByOptions() {
   //   return Object.keys(sortByOptions).map(sortByOption => {
@@ -15,29 +15,26 @@ class SearchBar extends React.Component {
   //     return <li key={sortByOptionValue}>{SortByOption}</li>
   //   })
   // }
-  renderSortByOptions() {
+  const renderSortByOptions = () => {
     return Object.keys(sortByOptions).map(sortByOption => <li key={sortByOptions[sortByOption]}>{sortByOption}</li>)
   }
 
-  
-  render() {
     return (
-      <div className="SearchBar">
-        <div className="SearchBar-sort-options">
-          <ul>
-            {this.renderSortByOptions()}
-          </ul>
-        </div>
-        <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
-        </div>
-        <div className="SearchBar-submit">
-          <a href="#">Let's Go</a>
-        </div>
+    <div className="SearchBar">
+      <div className="SearchBar-sort-options">
+        <ul>
+          {renderSortByOptions()}
+        </ul>
       </div>
-    )
-  }
+      <div className="SearchBar-fields">
+        <input placeholder="Search Businesses" />
+        <input placeholder="Where?" />
+      </div>
+      <div className="SearchBar-submit">
+        <a href="#">Let's Go</a>
+      </div>
+    </div>
+  )
 }
 
 export default SearchBar
